6ff15e8ddbd4b1bd342c4c05ba59b9d238406794
Louan-cbzls <cubizolles.louan@gmail.com>
2024-10-29 15:42:15 +0100

    X Y Z P T

--- Louan.py
+++ Louan.py
@@ -1,1 +1,54 @@
-EH coucou
+import numpy as np
+
+def positionX():
+    lettre_X = np.array([[0,1,0],[1,1,1],[0,1,0]])
+    return(ettre_X)
+
+def positionY():
+    lettre_Y = np.array([[0,0,0,0],[0,0,0,0],[0,0,1,0],[1,1,1,1]])
+    return(ettre_Y)
+
+def positionZ():
+    lettre_Z = np.array([[1,1,0],[1,1,1],[0,1,1]])
+    return(ettre_Z)
+
+def positionP():
+    lettre_P = np.array([[1,1,0],[1,1,0],[1,0,0]])
+    return(ettre_P)
+
+def positionT():
+    lettre_T = np.array([[1,1,1],[0,1,0],[0,1,0]])
+    return(ettre_T)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

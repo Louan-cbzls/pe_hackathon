7659e47385cc6f24cda3c3bf1f8d8147697db2f1
Baptiste Michaud <bmichaud_05@outlook.fr>
2024-10-29 15:42:04 +0100

    ajout fonction rotation et symetrie

--- common_baptiste_.py
+++ common_baptiste_.py
@@ -1,6 +1,15 @@
 n = 6
 m = 10
 
+Lettres_Base = [lettre_f,lettre_i,lettre_l,lettre_n,lettre_p,lettre_t,lettre_u,lettre_v,lettre_w,lettre_x,lettre_y,lettre_z]
+
 def grille(n,m):
     tab = np.zeros((n,m))
     return(tab)
+
+
+def rotation(tab):
+    return(numpy.rot90(tab))
+
+def symetrie(tab):
+    return(numpy.fliplr(tab))
